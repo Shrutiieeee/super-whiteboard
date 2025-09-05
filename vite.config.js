e4122050e220b3f44c9ee745d303f3a39dd0e677
Shrutiieeee <shrutikakhandale@gmail.com>
2025-09-05 13:52:24 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_ZEGO_APP_ID || "/super-whiteboard", 
+  base: process.env.VITE_ZEGO_APP_ID || "/super-whiteboard",
+  base:process.env.VITE_ZEGO_SERVER_URL || "/super-whiteboard", 
   build: {
     outDir: 'dist',
   }
